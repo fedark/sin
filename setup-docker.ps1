067a484d6a0ada7bd6a84043286af63c2f54a8fc
fedark <fedark@github.com>
2022-01-30 14:10:25 +0300

    Added a setup script for windows.

---
+++ setup-docker.ps1
@@ -0,0 +1,7 @@
+﻿docker build -t fedark/sin .
+
+docker network create sin
+
+docker run -d --name es --net sin -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" elasticsearch:7.16.3
+
+docker run -d --name van --net sin -p 5000:5000 fedark/sin
